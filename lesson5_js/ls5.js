9b69bb4c32750f72e8b47bbbcb4ced4a66361939
NataSereda <natalya.romanyak@gmail.com>
2021-07-27 21:18:46 +0300

    task

--- lesson5_js/ls5.js
+++ lesson5_js/ls5.js
@@ -1,19 +1,51 @@
-let content = document.getElementById(`content`);
-console.log(content);
+/*// let content = document.getElementById(`content`);*/
+// console.log(content);
+// content.innerText = `it's raining day`;
+// content.style.backgroundColor = `red`;
+// content.style.color = `blue`;
+//
+// let rule = document.getElementById(`rules`);
+// console.log(rule);
+// rule.innerText = `Since the beginning of time, flowers have intrigued us with their unique beauty and enticing scents. But some of these exceptional ‘gifts of nature’ possess unbelievable characteristics that are unknown to many of us. Here are some strange facts about flowers, both rare breeds and those we see regularly`
+// rule.style.backgroundColor = `red`;
+// rule.style.color = `blue`;
 
-let rules = document.getElementById(`rules`);
-console.log(rules);
 
-let one = document.getElementsByClassName(` fc_rules `);
-console.log(one)
+        
 
 
-content.innerText = `it's raining day`;
-rules.innerText = `Since the beginning of time, flowers have intrigued us with their unique beauty and enticing scents. But some of these exceptional ‘gifts of nature’ possess unbelievable characteristics that are unknown to many of us. Here are some strange facts about flowers, both rare breeds and those we see regularly`
+/*
+let fc_rules = document.getElementsByClassName(`fc_rules`);
+for (const fcRule of fc_rules) {
 
-content.style.backgroundColor = `red`;
-rules.style.backgroundColor = `red`;
-content.style.color = `blue`;
-rules.style.color = `blue`;
+    fcRule.style.backgroundColor = 'red';
+    fcRule.style.color = `blue`;
+    console.log(fcRule);
+};*/
+/*отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний текст*/
+
+let fc_rules = document.getElementsByClassName(`fc_rules`);
+for (const fcRule of fc_rules) {
+    fcRule.addEventListener(`click`,(evt => {
+        console.log (fcRule.textContent)
+    }))
+    fcRule.addEventListener(`click`,(evt => {
+        console.log ("kjknjkn")
+    }))
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ lesson5_js/practic5.js
@@ -0,0 +1,10 @@
+/*змінює колір тексту елемeнту з ід main_header на назву групи в якій ви вчитесь (mon-year))*/
+
+let header= document.getElementById(`main_header`);
+header.innerText = `June_2021`;
+
+
+
+let link = document.getElementsByTagName(`li`);
+console.log(link);
+
