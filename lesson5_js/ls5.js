18bd407f37dd74bcd811c2d731f16f2f7c9dd6f8
NataSereda <natalya.romanyak@gmail.com>
2021-07-27 10:46:42 +0300

    Initial commit

---
+++ lesson5_js/ls5.js
@@ -0,0 +1,19 @@
+let content = document.getElementById(`content`);
+console.log(content);
+
+let rules = document.getElementById(`rules`);
+console.log(rules);
+
+let one = document.getElementsByClassName(` fc_rules `);
+console.log(one)
+
+
+content.innerText = `it's raining day`;
+rules.innerText = `Since the beginning of time, flowers have intrigued us with their unique beauty and enticing scents. But some of these exceptional ‘gifts of nature’ possess unbelievable characteristics that are unknown to many of us. Here are some strange facts about flowers, both rare breeds and those we see regularly`
+
+content.style.backgroundColor = `red`;
+rules.style.backgroundColor = `red`;
+content.style.color = `blue`;
+rules.style.color = `blue`;
+
+
