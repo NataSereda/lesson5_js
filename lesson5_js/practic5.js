f7c96370c0fd340ba888004d9f8f591248153532
NataSereda <natalya.romanyak@gmail.com>
2021-07-28 16:18:34 +0300

    new

--- lesson5_js/practic5.js
+++ lesson5_js/practic5.js
@@ -1,10 +1,30 @@
 /*змінює колір тексту елемeнту з ід main_header на назву групи в якій ви вчитесь (mon-year))*/
 
-let header= document.getElementById(`main_header`);
-header.innerText = `June_2021`;
+/*let header= document.getElementById(`main_header`);
+header.innerText = `June_2021`;*/
+/*for (const headerElement of header) {
+    headerElement.style. wi
 
+}*/
 
+/*робить шириниу елементу ul 400px*/
 
-let link = document.getElementsByTagName(`li`);
-console.log(link);
+/*let ul = document.getElementsByTagName(`ul`)
+for (const ulElement of ul) {
+    ulElement.style.width = `400px`;
+}*/
 
+/*робить шириниу всіх елементів з класом linkList шириною 50%*/
+
+/*let LinkList  = document.getElementsByTagName(`li`)
+for (const linkListElement of LinkList) {
+    linkListElement.style.width = `50%`
+
+}*/
+
+/*отримує текст який зберігається в елементі з класом listElement2*/
+
+let List = localStorage.getItem('linkList listElement2');
+console.log(List)
+
+let LinkList2 = document.forms.formForlistElement2
